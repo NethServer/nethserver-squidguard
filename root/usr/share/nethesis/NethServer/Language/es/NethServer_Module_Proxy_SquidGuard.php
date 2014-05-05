b6c32c7185e7ed206f30e8e466f4bb19ee4b904d
Victor Moreno <victor_moreno_rubio@hotmail.com>
2014-05-05 16:30:29 +0200

    Web interface: add Spanish translation. Refs #2732

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_Proxy_SquidGuard.php
@@ -0,0 +1,43 @@
+<?php 
+
+/* NethServer_Module_Proxy_SquidGuard translation, language: es */
+
+$L['Add allowed domain'] = 'Añadir dominio permitido';
+$L['Add allowed URL'] = 'Añadir URL permitido';
+$L['Add blocked domain'] = 'Agregar dominios bloqueados';
+$L['Add blocked IP'] = 'Agregar bloqueo IP';
+$L['Add blocked URL'] = 'Agregar bloqueo URL';
+$L['Add unfiltered IP'] = 'Añadir IP sin filtrar';
+$L['ads'] = 'Publicidad';
+$L['aggressive'] = 'Aggresivo';
+$L['Allow domain'] = 'Permitir dominio';
+$L['AllowedCategories_label'] = 'Categorias permitidas';
+$L['Allow_label'] = 'Permitir a los sitios y direcciones IP';
+$L['Allow URL'] = 'Permitir URL';
+$L['audio-video'] = 'Audio y video';
+$L['BlockAll_label'] = 'Modo';
+$L['Block domain'] = 'Bloquear dominio';
+$L['BlockedCategories_label'] = 'Categorias bloqueadas';
+$L['BlockedFileTypes_label'] = 'Lista de bloqueaos de extensión de archivo';
+$L['BlockIpAccess_label'] = 'Bloquear el acceso a sitios Web utilizando la dirección IP';
+$L['Block_label'] = 'Bloquear los sitios y direcciones IP';
+$L['Block source IP'] = 'Bloquear IP de origen';
+$L['Block URL'] = 'Bloquear URL';
+$L['disabled_label'] = 'Permitir todas, bloque los contenidos seleccionados';
+$L['Do not filter source IP'] = 'No filtrar IP de origen';
+$L['drugs'] = 'Drogas';
+$L['enabled_label'] = 'Bloquear todo, permitir el contenido seleccionado';
+$L['Expressions'] = 'Expresiones';
+$L['Expressions_label'] = 'Habilitar expresión URL';
+$L['gambling_label'] = 'Juego';
+$L['hacking'] = 'Hacking';
+$L['mail'] = 'Correo';
+$L['porn'] = 'Porno';
+$L['proxy'] = 'Proxy';
+$L['redirector'] = 'Redirector';
+$L['spyware'] = 'Spyware';
+$L['SquidGuard_Title'] = 'Filtro';
+$L['status_label'] = 'Activar filtro';
+$L['suspect'] = 'Sospechoso';
+$L['violence'] = 'Violencia';
+$L['warez'] = 'Warez';
