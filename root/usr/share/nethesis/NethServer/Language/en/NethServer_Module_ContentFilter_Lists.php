c37e70ad4c941f09cce8e8ad46c7e53fa2737f00
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2014-11-24 16:40:15 +0100

    Web UI: download blacklists in background. Refs #2959

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_ContentFilter_Lists.php
@@ -0,0 +1,13 @@
+<?php
+
+$L['Lists_Title'] = 'Blacklists';
+$L['Lists_selector'] = 'Available lists';
+$L['Lists_shalla_label'] = 'Shalla (free for non-commercial use)';
+$L['Lists_urlblacklist_label'] = 'UrlBlacklist.com (commercial)';
+$L['Lists_toulouse_label'] = 'Université Toulouse (free)';
+$L['Lists_custom_label'] = 'Custom';
+$L['CustomListURL_label'] = 'Custom URL';
+$L['Download_label'] = 'Download';
+$L['Download_header'] = 'Download';
+$L['Download_message'] = 'The process can take a while. Proceed with download?';
+$L['save_and_download_label'] = 'Save and Download';
--- root/usr/share/nethesis/NethServer/Template/ContentFilter/Lists/Select.php
+++ root/usr/share/nethesis/NethServer/Template/ContentFilter/Lists/Select.php
@@ -6,7 +6,7 @@ echo $view->fieldset()->setAttribute('template', $T('Lists_selector'))
     ->insert($view->radioButton('Lists', 'toulouse'))
     ->insert($view->fieldsetSwitch('Lists', 'custom')
         ->setAttribute('uncheckedValue', '')
-        ->insert($view->textInput('server')));
+        ->insert($view->textInput('CustomListURL')));
 
 echo $view->buttonList()
     ->insert($view->button('save_and_download', $view::BUTTON_SUBMIT))
