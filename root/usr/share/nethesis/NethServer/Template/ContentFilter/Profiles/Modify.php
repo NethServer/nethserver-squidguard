bc8c63c2a641ce2c5550c2a959f4120613add0fb
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2014-11-25 09:46:09 +0100

    Web UI: add timepicker. Refs #1984

--- root/usr/share/nethesis/NethServer/Template/ContentFilter/Profiles/Modify.php
+++ root/usr/share/nethesis/NethServer/Template/ContentFilter/Profiles/Modify.php
@@ -6,4 +6,4 @@ echo $view->selector('Src', $view::SELECTOR_DROPDOWN);
 echo $view->selector('Filter');
 echo $view->selector('Time');
 
-echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
+echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
--- root/usr/share/nethesis/NethServer/Template/ContentFilter/Times/Modify.php
+++ root/usr/share/nethesis/NethServer/Template/ContentFilter/Times/Modify.php
@@ -1,10 +1,24 @@
 <?php
+$view->includeFile('NethServer/Js/jquery.timepicker.min.js');
+$view->includeFile('NethServer/Css/jquery.timepicker.css');
 
 echo $view->textInput('name');
 echo $view->textInput('Description');
 echo $view->selector('Days', $view::SELECTOR_MULTIPLE);
 echo $view->columns()
-    ->insert($view->selector('StartTime', $view::SELECTOR_DROPDOWN))
-    ->insert($view->selector('EndTime', $view::SELECTOR_DROPDOWN));
+    ->insert($view->textInput('StartTime'))
+    ->insert($view->textInput('EndTime'));
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
+
+$start_id = $view->getClientEventTarget('StartTime');
+$end_id = $view->getClientEventTarget('EndTime');
+
+$view->includeJavascript("
+(function ( $ ) {
+    $(document).ready(function() {
+        $('.$start_id').timepicker( { minTime: \"00:00\", timeFormat: \"\H:\i\"} ); 
+        $('.$end_id').timepicker( { minTime: \"00:00\", timeFormat: \"\H:\i\"} );
+    });
+})( jQuery );
+");
