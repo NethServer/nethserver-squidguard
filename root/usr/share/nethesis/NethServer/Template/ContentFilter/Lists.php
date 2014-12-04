b31769996cfc0e02b9762e6660e23815b7d25dc9
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2014-12-04 15:22:07 +0100

    Web UI: remove download button. Refs #2959
    The download is always started even if the selected
    list doesn't change.

--- root/usr/share/nethesis/NethServer/Template/ContentFilter/Lists.php
+++ root/usr/share/nethesis/NethServer/Template/ContentFilter/Lists.php
@@ -5,11 +5,10 @@ echo $view->fieldset()->setAttribute('template', $T('Lists_selector'))
     ->insert($view->radioButton('Lists', 'urlblacklist'))
     ->insert($view->radioButton('Lists', 'toulouse'))
     ->insert($view->fieldsetSwitch('Lists', 'custom')
-        ->setAttribute('uncheckedValue', 'disabled')
-        ->insert($view->textInput('server')));
+        ->setAttribute('uncheckedValue', '')
+        ->insert($view->textInput('CustomListURL')));
 
 echo $view->buttonList()
-    ->insert($view->button('Save', $view::BUTTON_SUBMIT))
-    ->insert($view->button('Download', $view::BUTTON_LINK)->setAttribute('value', $view->getModuleUrl('../Lists/Download')))
+    ->insert($view->button('save_and_download', $view::BUTTON_SUBMIT))
     ->insert($view->button('Help', $view::BUTTON_HELP))
 ;
--- root/usr/share/nethesis/NethServer/Template/ContentFilter/Lists/Download.php
+++
@@ -1,6 +0,0 @@
-<?php
-$view->requireFlag($view::INSET_DIALOG);
-
-echo $view->header()->setAttribute('template', $T('Download_header'));
-echo $view->translate('Download_message');
-echo $view->buttonList($view::BUTTON_CANCEL | $view::BUTTON_SUBMIT);
