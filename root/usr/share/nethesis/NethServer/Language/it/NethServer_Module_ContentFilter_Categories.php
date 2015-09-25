2db828cb9c85ad928d3fa4d1cab2b0cc7c627033
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2015-09-25 10:48:00 +0200

    Remove Italian translation. Refs #3265

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_ContentFilter_Categories.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$L['Categories_Title'] = 'Categorie personalizzate';
-$L['name_label'] = 'Nome';
-$L['Description_label'] = 'Descrizione';
-$L['Domains_label'] = 'Domini';
-$L['Key_label'] = 'Nome';
